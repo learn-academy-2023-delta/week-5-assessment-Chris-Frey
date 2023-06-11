aa2781d53b31c913c49f0028a70529e59208d0b4
Chris-Frey <chris.frey@live.com>
2023-06-10 19:28:46 -0700

    week 5 assessment finished

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,7 +13,13 @@
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
-
+  describe("codeBreaker", () =>{
+    it("takes a string and returns a decoded message", () => {
+      expect(codeBreaker(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(codeBreaker(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(codeBreaker(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 const secretCodeWord2 = "Gobbledygook"
@@ -22,12 +28,43 @@ const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 // b) Create the function that makes the test pass.
+const codeBreaker = (str) => {
+  let arr = str.split('')
+  let new_arr = arr.map(letter => {
+          if(letter.toLowerCase() == 'a') {
+            return letter = "4"
+          }else if(letter.toLowerCase() === "e") {
+            return letter = "3"
+          }else if(letter.toLowerCase() === "i") {
+            return letter = "1"
+          }else if(letter.toLowerCase() === "o"){
+            return letter = "0"
+          }else{
+            return letter
+          }
+        }
+  )
+  return new_arr.join('')
+}
 
+
+// codeBreaker(secretCodeWord1);
 // Pseudo code:
+// create an arrow function that takes in a string argument
+// split the argument into an array of letters and assign that array to a variable
+// iterate over the variable with map
+  // check to see if letters match criteria. if they do, then replace with new strings
+//join the mapped array
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+describe("fruity", () =>{
+  it("takes an array and a letter, and returns words that have that letter", () => {
+    expect(fruity(fruitArray, filterLetterA)).toEqual(["Mango", "Apricot", "Peach"])
+    expect(fruity(fruitArray, filterLetterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+  })
+})
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -37,12 +74,29 @@ const filterLetterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
+const fruity = (arr, letter) => {
+  let new_arr = arr.filter(input => {
+    return (input.toLowerCase()).includes(letter)
+  })
+  return new_arr
+}
+// Pseudo code:
+// create arrow function with 2 inputs
+// filter over the array
+//  check to see if the words include a letter. if so, filter moves them to `new_arr`
+// return new_arr
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
-// Pseudo code:
-
 // a) Create a test with expect statements using the variable provided.
+describe("cards", () =>{
+  it("takes an array of numbers and returns true if numbers make a full house", () => {
+    expect(cards(hand1)).toEqual(true)
+    expect(cards(hand2)).toEqual(false)
+    expect(cards(hand3)).toEqual(false)
+    expect(cards(hand4)).toEqual(true)
+  })
+})
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -54,5 +108,29 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+const cards = (hand) => {
+  let handCheck = {}
 
+  for(const element of hand) {
+    // console.log(element);
+    if(handCheck[element]) {
+      handCheck[element] += 1
+    } else {
+      handCheck[element] = 1
+    }
+  }
+
+  if(Object.keys(handCheck).length !== 2) {
+    return false
+  } else if(Object.values(handCheck).includes(3) && Object.values(handCheck).includes(2)) {
+    return true
+  } else {
+    return false
+  }
+}
 // Pseudo code:
+// create arrow function that takes an array
+// create a variable to hold the return from the for-of loop
+// iterate over the array with for-of loop
+// use conditional statement to check if the keys of the variable are greater than two, and if they include both the numbers 2 and 3
+// everything else is false
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,23 +10,68 @@ filter_letter_o = 'o'
 filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def find_words(arr, filter_letter)
+  arr.select do |word|
+    word.include?(filter_letter)
+  end
+end
+
+p find_words(beverages_array, filter_letter_o) == ['coffee', 'soda water']
+p find_words(beverages_array, filter_letter_t) == ['tea', 'water', 'soda water']
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
+def iterated_hashes(hsh)
+  hsh.values.flatten.sort
+end
 
+p iterated_hashes(us_states) == ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+class Bike
+  attr_accessor :model, :current_speed, :wheels
+  def initialize(model, current_speed) 
+    @model = model
+    @wheels = 2
+    @current_speed = current_speed
+  end
+
+  def bike_info
+   "The #{model} bike has #{wheels} wheels and is going #{current_speed} mph."
+  end
 
 
+  def pedal_faster(speed)
+    self.current_speed += speed
+  end
 
+  def brake(speed)
+    if (self.current_speed -= speed) < 0
+      return 0
+    else
+      self.current_speed
+    end
+  end
+  #***Please note that I did not include the '@' symbol on the instance variables, intentionally. My understanding of Ruby tells me that we only want to have an instance variable in one area, the getter/setter methods. This way we are using the method definition, and not the variable itself. So when code changes are made (for instance if we need to add a default idle speed to the `@current_speed` instance variable, or something), we don't have to go through all the `@current_speed` variables in the code. We can just update the method, and it trickles down to the rest of the method calls.  
+  #Not using the '@' symbol, and using the keyword `self` automatically asks Ruby to look at the getter and setter methods for the variable definition.***
+end
+
+chris_bike = Bike.new("Treck", 0)
+p chris_bike.bike_info
+# puts chris_bike #<-this also returns the same as line 65
+my_bike = Bike.new("Mongoose", 0)
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
-# Expected output example: my_bike.pedal_faster(10) => 10
-# Expected output example: my_bike.pedal_faster(18) => 28
-# Expected output example: my_bike.brake(5) => 23
-# Expected output example: my_bike.brake(25) => 0
+# Expected output example: my_bike.pedal_faster(10) #=> 10
+# Expected output example: my_bike.pedal_faster(18) #=> 28
+# Expected output example: my_bike.brake(5) #=> 23
+# Expected output example: my_bike.brake(25) #=> 0
+p my_bike.pedal_faster(10) #=> 10
+p my_bike.pedal_faster(18) #=> 28
+p my_bike.brake(5) #=> 23
+p my_bike.brake(25) #=> 0
